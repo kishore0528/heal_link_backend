cb682f7057946fa465d3d0b02f1f3a875741d7d0
kamal-2006 <kamaleshkamales006@gmail.com>
2025-09-20 23:10:29 +0530

    message

--- models/User.js
+++ models/User.js
@@ -62,12 +62,11 @@ const UserSchema = new mongoose.Schema({
 // Encrypt password using bcrypt
 UserSchema.pre('save', async function (next) {
   if (!this.isModified('password') || !this.password) {
-    return next();
+    next();
   }
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // Sign JWT and return
