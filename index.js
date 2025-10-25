f4be4a4bc57790366be71b128c7f03ad9fdedb83
kamal-2006 <kamaleshkamales006@gmail.com>
2025-10-25 23:58:05 +0530

    corrected error

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 // Enable CORS for frontend origin
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001", process.env.FRONTEND_ORIGIN].filter(
+    origin: ["http://localhost:3000", "https://project-heal-link.vercel.app/", process.env.FRONTEND_ORIGIN].filter(
       Boolean
     ),
     credentials: true,
